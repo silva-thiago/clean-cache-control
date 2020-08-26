a585fcfa801ba0451c8265dedd96422acdb2b3ae
thiagosilva <thiago.silva@imd.ufrn.br>
2020-08-26 01:33:56 -0300

    Nome do arquivo corrigido + Módulo de extensões de arquivos

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   roots: ['<rootDir>/src'],
+  preset: 'ts-jest',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
   },
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1'
-  }
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
 }
