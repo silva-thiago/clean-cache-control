a9814a61ac334d90571bbb1abc09fa6276523771
thiagosilva <thiago.silva@imd.ufrn.br>
2020-08-26 01:12:57 -0300

    Criação SUT modificado para um método auxiliar

--- src/domain/data/usescases/local-save-purchases.spec.ts
+++ src/domain/data/usescases/local-save-purchases.spec.ts
@@ -18,16 +18,29 @@ class CacheStoreSpy implements CacheStore {
   }
 }
 
+type SUTTypes = {
+  sut: LocalSavePurchases
+  cacheStore: CacheStoreSpy
+}
+
+const makeSUT = (): SUTTypes => {
+  const cacheStore = new CacheStoreSpy()
+  const sut = new LocalSavePurchases(cacheStore)
+
+  return {
+    sut,
+    cacheStore
+  }
+}
+
 describe('LocalSavePurchases', () => {
   test('Should not delete cache in sut.init', () => {
-    const cacheStore = new CacheStoreSpy()
-    new LocalSavePurchases(cacheStore)
+    const { cacheStore } = makeSUT()
     expect(cacheStore.deleteCallsCount).toBe(0)
   })
 
   test('Should delete old cache in sut.save', async () => {
-    const cacheStore = new CacheStoreSpy()
-    const sut = new LocalSavePurchases(cacheStore)
+    const { sut, cacheStore } = makeSUT()
     await sut.save()
     expect(cacheStore.deleteCallsCount).toBe(1)
   })
