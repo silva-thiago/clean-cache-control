b5c8cb0175ad8a97228aeb38834d380ba9059305
thiagosilva <thiago.silva@imd.ufrn.br>
2020-08-27 19:50:55 -0300

    Garantir que o LocalLoadPurchases não sofra danos se o carregamento falhar

--- src/data/usescases/load-purchases/local-load-purchases.ts
+++ src/data/usescases/load-purchases/local-load-purchases.ts
@@ -21,11 +21,11 @@ export class LocalLoadPurchases implements SavePurchases, LoadPurchases {
       if (CachePolicy.validate(cache.timestamp, this.currentDate)) {
         return cache.value
       } else {
-        throw new Error()
+        this.cacheStore.delete(this.key)
+
+        return []
       }
     } catch (error) {
-      this.cacheStore.delete(this.key)
-
       return []
     }
   }
