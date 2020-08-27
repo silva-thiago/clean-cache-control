7c9f78398df77904c411656f94f7458ccd34064f
thiagosilva <thiago.silva@imd.ufrn.br>
2020-08-27 15:07:10 -0300

    Teste simplificado com auxiliar

--- src/data/usescases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usescases/save-purchases/local-save-purchases.spec.ts
@@ -19,6 +19,10 @@ class CacheStoreSpy implements CacheStore {
     this.insertKey = key
     this.insertValues = value
   }
+
+  simulateDeleteError(): void {
+    jest.spyOn(CacheStoreSpy.prototype, 'delete').mockImplementationOnce(() => { throw new Error() })
+  }
 }
 
 const mockPurchases = (): Array<SavePurchases.Params> => [
@@ -63,7 +67,7 @@ describe('LocalSavePurchases', () => {
 
   test('Should not insert new cache if delete fails', () => {
     const { sut, cacheStore } = makeSUT()
-    jest.spyOn(cacheStore, 'delete').mockImplementationOnce(() => { throw new Error() })
+    cacheStore.simulateDeleteError()
     const promise = sut.save(mockPurchases())
     expect(cacheStore.insertCallsCount).toBe(0)
     expect(promise).rejects.toThrow()
